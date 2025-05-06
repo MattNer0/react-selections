03a920a792b504110da3cdaba13df08c30b3af01
Matteo <matteo.balloco@gmail.com>
2025-05-06 16:00:06 +0200

    moved index for easy import

---
+++ index.js
@@ -0,0 +1,11 @@
+import { withContainer } from './src/components/SelectionWrapper';
+import SelectionContainer from './src/components/SelectionContainer';
+import Selection from './src/components/SelectionFacade';
+import AbstractSelection from './src/components/AbstractSelection';
+
+export {
+  Selection,
+  SelectionContainer,
+  AbstractSelection,
+  withContainer,
+};
